c5566591a0dd49e95c8a918e0df34f5caedef540
joe2141 <elsuperjoy@gmail.com>
2023-03-20 18:49:01 -0600

    priemr commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { PageWrapperComponent } from './page-wrapper/page-wrapper.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { ToolbarComponent } from './toolbar/toolbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    PageWrapperComponent,
+    SidebarComponent,
+    ToolbarComponent
   ],
   imports: [
     BrowserModule
